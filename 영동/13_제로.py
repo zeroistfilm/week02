f478d77f7a101bb18f37ca1e25a88e4784f2b15d
zeroistfilm <zeroistfilm@naver.com>
2020-12-20 12:55:52 +0900

    yd

--- 영동/13_제로.py
+++ 영동/13_제로.py
@@ -1,1 +1,18 @@
 # https://www.acmicpc.net/problem/10773
+
+
+N = int(input())
+
+orders=[]
+for i in range(N):
+    orders.append(int(input()))
+
+result=[]
+for i in orders:
+
+    if i==0:
+        result.pop()
+    else:
+        result.append(i)
+
+print(sum(result))
