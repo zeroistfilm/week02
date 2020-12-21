4bc588fb9fda3f5c7710af811496c2c7314df22b
zeroistfilm <zeroistfilm@naver.com>
2020-12-21 10:28:45 +0900

    yd

---
+++ 영동/20_큐 2.py
@@ -0,0 +1,56 @@
+# https://www.acmicpc.net/problem/18258
+
+from collections import deque
+import sys
+class qqq():
+    def __init__(self):
+        self.queue=deque()
+
+    def push(self,n):
+        self.queue.append(n)
+
+    def pop(self):
+        if self.queue:
+            return self.queue.popleft()
+        else:
+            return -1
+    def size(self):
+        return len(self.queue)
+
+    def empty(self):
+        if self.queue:
+            return 0
+        else:
+            return 1
+
+    def front(self):
+        if not self.queue:
+            return -1
+        else:
+            return self.queue[0]
+    def back(self):
+        if not self.queue:
+            return -1
+        else:
+            return self.queue[-1]
+
+
+queue=qqq()
+N = int(input())
+orders=[]
+for i in range(N):
+    orders.append(list(map(str,sys.stdin.readline().split())))
+
+for order in orders:
+    if order[0]=='push':
+        queue.push(order[1])
+    elif order[0] == 'pop':
+        print(queue.pop())
+    elif order[0] == 'size':
+        print(queue.size())
+    elif order[0] == 'empty':
+        print(queue.empty())
+    elif order[0] == 'front':
+        print(queue.front())
+    elif order[0] == 'back':
+        print(queue.back())
---
+++ 영동/21_카드2.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/2164
+from collections import deque
+
+
+N = int(input())
+queue=deque()
+for i in range(1,N+1):
+    queue.append(i)
+
+
+while len(queue)!=1: #1이 아닐때 반복
+    if len(queue)==1:
+        break
+    else:
+        queue.popleft()
+        queue.append(queue.popleft())
+
+
+
+print(queue[0])
