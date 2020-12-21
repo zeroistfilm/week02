7fff891d86545a0a256a2e81296da158c8186182
zeroistfilm <zeroistfilm@naver.com>
2020-12-21 19:24:50 +0900

    yd

--- 영동/25_가운데를 말해요.py
+++ 영동/25_가운데를 말해요.py
@@ -1,17 +1,51 @@
 # https://www.acmicpc.net/problem/1655
-# 내풀이 주어진 배열을 복사, 삭제하는 과정은 효율적이지 않다
+# 내풀이 주어진 배열을 복사, 삭제하는 과정은 효율적이지 않
+# import heapq
+# import sys
+# N = int(sys.stdin.readline())
+#
+# minheap = []
+# j=0
+# for i in range(N):
+#     x = int(sys.stdin.readline())
+#     heapq.heappush(minheap, x)
+#     tmp=minheap[:]
+#     for k in range(j):
+#        heapq.heappop(tmp)
+#     print(heapq.heappop(tmp))
+#     if len(minheap)%2==0:
+#         j+=1
+
+#====================
 import heapq
 import sys
 N = int(sys.stdin.readline())
+minheap=[]
+maxheap=[]
 
-minheap = []
-j=0
 for i in range(N):
-    x = int(sys.stdin.readline())
-    heapq.heappush(minheap, x)
-    tmp=minheap[:]
-    for k in range(j):
-       heapq.heappop(tmp)
-    print(heapq.heappop(tmp))
-    if len(minheap)%2==0:
-        j+=1
+    x = int(sys.stdin.readline().strip())
+
+    if i%2!=0: #짝수번째로 들어오는것
+
+        heapq.heappush(minheap, (x,x))
+    else:
+
+        heapq.heappush(maxheap,(-x,x))
+
+    if i==0:
+        print(maxheap[0][1])
+    else:
+        if minheap[0][1] > maxheap[0][1]:
+            print(maxheap[0][1])
+        else:
+            maxhh= heapq.heappop(maxheap)
+            minhh= heapq.heappop(minheap)
+            heapq.heappush(minheap,(maxhh[1],maxhh[1]))
+            heapq.heappush(maxheap, (-minhh[1],minhh[1]))
+
+            print(maxheap[0][1])
+
+
+
+
