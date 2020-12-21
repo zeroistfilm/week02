b82f7096b3b6bb0fecce72beddf94f429111516a
zeroistfilm <zeroistfilm@naver.com>
2020-12-21 18:49:46 +0900

    yd

--- 영동/25_가운데를 말해요.py
+++ 영동/25_가운데를 말해요.py
@@ -1,8 +1,7 @@
 # https://www.acmicpc.net/problem/1655
-# 내풀이
+# 내풀이 주어진 배열을 복사, 삭제하는 과정은 효율적이지 않다
 import heapq
 import sys
-import copy
 N = int(sys.stdin.readline())
 
 minheap = []
@@ -10,7 +9,7 @@ j=0
 for i in range(N):
     x = int(sys.stdin.readline())
     heapq.heappush(minheap, x)
-    tmp=copy.deepcopy(minheap)
+    tmp=minheap[:]
     for k in range(j):
        heapq.heappop(tmp)
     print(heapq.heappop(tmp))
