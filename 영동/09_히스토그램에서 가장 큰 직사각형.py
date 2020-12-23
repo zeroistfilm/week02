a3a24e4c8a1316433d0dc704090e7363e48d4212
zeroistfilm <zeroistfilm@naver.com>
2020-12-23 14:34:14 +0900

    yd

--- 영동/09_히스토그램에서 가장 큰 직사각형.py
+++ 영동/09_히스토그램에서 가장 큰 직사각형.py
@@ -1,44 +1,79 @@
 # https://www.acmicpc.net/problem/6549
 # &1 메모리 초과 : Q: 메모리 초과가 나오는것은 리스트의 문제인가??
 # https://bladejun.tistory.com/11 스택을 이용한  풀이
-import sys
-from collections import deque
 
-
-maxarea=0
-def divide_search(array):
-    start=0
-    end=len(array)
-    global maxarea
-
-    if len(array)==1:
-        area=array[0]
-        if area > maxarea:
-            maxarea = area
-        return 0
-    if len(array)<1:
-        return 0
-
-    minumumvalue=min(array)
-    mid = array.index(minumumvalue)  # 처음 케이스에서 인덱스 1이 반환됨
-    range=len(array)
-    area = minumumvalue*range
-    if area>maxarea:
-        maxarea=area
-    divide_search(array[start:mid])
-    divide_search(array[mid+1:end])
 
 
 while True:
-    testcase = list(map(int,sys.stdin.readline().split()))
-    N=testcase[0]
-    if N==0:
+    N, *cols = list(map(int, input().split())) #*이 하나가 붙은 경우, 가변 변수로 설정한다.
+    cols.append(0)
+    if N==0:#종료조건
         break
-    heights=testcase[1:]
+    area=0
+    stack = []
+
+    for i, h in enumerate(cols): #인덱스와 값을 동시에 전달한다.
+        #print(i,h)
+        while stack and cols[stack[-1]]>h: #스텍 마지막에 쌓인 기둥이 현재 기둥보다 더 클 클때
+            ih=cols[stack.pop()]#현재 높이
+            w = i - stack[-1] - 1 if stack else i
+            if area < w * ih: area = w * ih
+        stack.append(i)
+    print(area)
 
 
-    divide_search(heights)
-    print(maxarea)
+
+
+
+
+
+
+
+
+
+
+
+
+
+import sys
+
+from collections import deque
+#
+#
+# maxarea=0
+# def divide_search(array):
+#     start=0
+#     end=len(array)
+#     global maxarea
+#
+#     if len(array)==1:
+#         area=array[0]
+#         if area > maxarea:
+#             maxarea = area
+#         return 0
+#     if len(array)<1:
+#         return 0
+#
+#     minumumvalue=min(array)
+#     mid = array.index(minumumvalue)  # 처음 케이스에서 인덱스 1이 반환됨
+#     range=len(array)
+#     area = minumumvalue*range
+#     if area>maxarea:
+#         maxarea=area
+#     divide_search(array[start:mid])
+#     divide_search(array[mid+1:end])
+#
+#
+# while True:
+#     testcase = list(map(int,sys.stdin.readline().split()))
+#     N=testcase[0]
+#     if N==0:
+#         break
+#     heights=testcase[1:]
+#
+#
+#     divide_search(heights)
+#     print(maxarea)
 
 
 
---
+++ 영동/practice4.py
@@ -0,0 +1,33 @@
+# https://programmers.co.kr/learn/courses/30/lessons/60060?language=python3
+from bisect import bisect_right, bisect_left
+
+def count_by_range(array, left, right):
+    leftindex = bisect_left(array, left)
+    rightindex = bisect_right(array, right)
+    return rightindex - leftindex
+
+array = [[] for _ in range(10001)]
+reverse_array = [[] for _ in range(10001)]
+
+def solution(words,queries):
+    answer=[]
+
+    for word in words:
+        array[len(word)].append(word)
+        reverse_array[len(word)].append(word[::-1])
+
+    for i in range(10001):
+        array[i].sort()
+        reverse_array[i].sort()
+
+    for q in queries:
+        if q[-1]=='?': #접미
+            res = count_by_range(array[len(q)],q.replace('?','a'),q.replace('?','z'))
+        else: #접두
+            res = count_by_range(reverse_array[len(q)], q[::-1].replace('?', 'a'), q[::-1].replace('?', 'z'))
+
+        answer.append(res)
+    return  answer
+
+
+solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?"])
